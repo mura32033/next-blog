37f5111cc3e516434fc62d415738517cbdba0f81
mura32033 <mura3-2033@hotmail.co.jp>
2022-09-18 01:48:36 +0900

    added custom font

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <header className="bg-slate-900">
       <div className="container mx-auto flex flex-row flex-wrap p-5 items-center">
-        <div className="font-medium text-lg items-center text-white">
+        <div className="font-sans font-medium text-sm items-center text-white">
           ©{new Date().getFullYear()} murasan.
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Noto Sans", "sans-serif"]
+    }
   },
   plugins: [],
 }
