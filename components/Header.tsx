87b738b5166768edff06082ea540b579c43d5208
mura32033 <mura3-2033@hotmail.co.jp>
2022-09-27 17:52:39 +0900

    emphasize current path in header navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,34 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+  const router = useRouter()
+  const link = [
+    {
+      name: 'Article',
+      path: '/article',
+    },
+    {
+      name: 'Profile',
+      path: '/profile',
+    }
+  ]
   return (
     <header className="bg-slate-900">
       <div className="container mx-auto flex flex-row flex-wrap p-5 items-center">
-        <Link href={'/'} passHref>
+        <Link href={'/'}>
           <a className="font-medium font-sans text-2xl items-center text-white">
             Blog
           </a>
         </Link>
         <nav className="flex flex-row ml-auto gap-4">
-          <Link href={'/article'} passHref>
-            <a className="font-medium font-sans items-center text-white">
-              Articles
-            </a>
-          </Link>
-          <Link href={'/profile'} passHref>
-          <a className="font-medium font-sans items-center text-white">
-            Profile
-          </a>
-        </Link>
+          {link.map((item) => {
+            return (
+              <Link key={item.name} href={item.path} passHref>
+                <a className={`font-sans items-center text-slate-100 hover:text-slate-200 ${router.pathname === item.path ? 'text-slate-200 font-medium underline' : ''}`}>{item.name}</a>
+              </Link>
+            )
+          })}
         </nav>
       </div>
     </header>
